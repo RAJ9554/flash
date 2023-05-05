85dddc93813115306be683370f04a85c8a8e197b
raj <rajkumaryadav212601@gmail.com>
2023-05-05 14:29:31 +0530

    First Commit uploading all files

---
+++ demo/src/dem/Basicjavaprogram.java
@@ -0,0 +1,12 @@
+package dem;
+
+public class Basicjavaprogram {
+
+	public static void main(String[] args) {
+		int num1= 2 , num2 = 3 ;
+		System.out.println(" Arithmetic sum of " +  (num1+num2) );
+		System.out.println("subtraction" + (num1-num2));
+		System.out.println("multiplication"+ (num1*num2));
+	}
+
+}
---
+++ demo/src/dem/Basicjavaprogramv2.java
@@ -0,0 +1,20 @@
+package dem;
+
+import java.util.Scanner;
+
+public class Basicjavaprogramv2 {
+
+	public static void main(String[] args) {
+		int num1,num2;
+		// TODO Auto-generated method stub
+		Scanner tc=  new Scanner(System.in);
+		//System.out.println("Enter first num");
+		num1=tc.nextInt();
+		//System.out.println("Enter second num");
+		num2=tc.nextInt();
+		System.out.println(num1+num2);
+		System.out.print(num1*num2);
+
+	}
+
+}
